69f7a24c74660c38ce52db88331da940dea822b1
chbog <83748023+chbog@users.noreply.github.com>
2022-05-02 12:24:41 +0300

    Initial commit

---
+++ src/test/java/com/example/timeregistration/TimeRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timeregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
